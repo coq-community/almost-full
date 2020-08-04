8f997d6f828f98b885bd47ec391e720e5757a165
Karl Palmskog <palmskog@gmail.com>
2020-08-04 15:07:57 -0500

    switch from Set to Type for PropBounded files

--- theories/PropBounded/AFConstructions.v
+++ theories/PropBounded/AFConstructions.v
@@ -22,7 +22,7 @@ Set Transparent Obligations.
 
 (*=AfUnion *)
 Corollary af_union: 
- forall (X:Set) (A B : X -> X -> Prop),
+ forall (X:Type) (A B : X -> X -> Prop),
  almost_full A -> almost_full (fun x y => A x y \/ B x y).
 (*=End *)
 intros X A B afA. eapply af_strengthen. apply afA. auto.
@@ -37,7 +37,7 @@ Defined.
  ****************************************************************)
 
 (*=OplusNullaryLemma *)
-Lemma oplus_nullary (X:Set) (A B : Prop) (R : X -> X -> Prop) : 
+Lemma oplus_nullary (X:Type) (A B : Prop) (R : X -> X -> Prop) : 
   almost_full R ->
   forall C, 
   (forall x y, R x y <-> C x y \/ A) ->
@@ -66,7 +66,7 @@ left. left. apply H3. right. right. apply H3.
 Defined.
 
 (*=OplusNullaryCor *)
-Lemma oplus_nullary_cor (X:Set) (A B : Prop) (C : X -> X -> Prop) : 
+Lemma oplus_nullary_cor (X:Type) (A B : Prop) (C : X -> X -> Prop) : 
   almost_full (fun x y => C x y \/ A) -> 
   almost_full (fun x y => C x y \/ B) -> 
   almost_full (fun x y => C x y \/ (A /\ B)).
@@ -76,7 +76,7 @@ apply H. auto. intros. firstorder. apply H0.
 Defined.
 
 (*=OplusUnaryLemma *)
-Lemma oplus_unary (X:Set) (A B : X -> Prop):
+Lemma oplus_unary (X:Type) (A B : X -> Prop):
   forall R, almost_full R -> 
   forall T, almost_full T -> 
   forall C, 
@@ -134,7 +134,7 @@ Defined.
 
 
 (*=OplusUnaryCor *)
-Lemma oplus_unary_cor (X:Set) (A B : X -> Prop) (C : X -> X -> Prop):
+Lemma oplus_unary_cor (X:Type) (A B : X -> Prop) (C : X -> X -> Prop):
   almost_full(fun x y => C x y \/ A x) ->
   almost_full(fun x y => C x y \/ B x) -> 
   almost_full (fun x y => C x y \/ (A x /\ B x)).
@@ -145,7 +145,7 @@ Defined.
 
 
 (*=OplusBinaryLemma *)
-Lemma oplus_binary (X:Set):
+Lemma oplus_binary (X:Type):
   forall A, almost_full A -> 
   forall B, almost_full B -> 
     @almost_full X (fun x y => A x y /\ B x y).
@@ -170,7 +170,7 @@ intros A afA. induction afA.
 Defined.
 
 (*=AfIntersection *)
-Corollary af_intersection (X:Set) (A B :X->X->Prop):
+Corollary af_intersection (X:Type) (A B :X->X->Prop):
   almost_full A -> almost_full B -> 
   almost_full (fun x y => A x y /\ B x y).
 (*=End *)
@@ -191,7 +191,7 @@ Defined.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
 
 (*=CoFmapLemma *)
-Corollary af_cofmap (X Y:Set) (f:Y->X) (R:X->X->Prop):
+Corollary af_cofmap (X Y:Type) (f:Y->X) (R:X->X->Prop):
   almost_full R -> almost_full (fun x y => R (f x) (f y)).
 (*=End *)
 intro afR. 
@@ -203,7 +203,7 @@ Defined.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
 
 (*=AfProduct *) 
-Lemma af_product (X : Set) (Y : Set) : 
+Lemma af_product (X : Type) (Y : Type) : 
   forall (A : X -> X -> Prop) (B : Y -> Y -> Prop), 
   almost_full A -> almost_full B -> 
   almost_full (fun x y => A (fst x) (fst y) /\ B (snd x) (snd y)).
@@ -214,7 +214,7 @@ apply(af_intersection (@af_cofmap _ _ (@fst X Y) A afA)
 Defined.
 
 (*=AfProductLeft *)
-Lemma af_product_left (X Y : Set) (A:X->X->Prop) : 
+Lemma af_product_left (X Y : Type) (A:X->X->Prop) : 
   almost_full A -> 
   almost_full (fun (x:X*Y) (y:X*Y) => A (fst x) (fst y)).
 intros afA. 
@@ -239,7 +239,7 @@ Defined.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
  
 (*=SumLift *)
-Definition sum_lift (X Y:Set) 
+Definition sum_lift (X Y:Type) 
   (A:X->X->Prop) (B:Y->Y->Prop) (x:X+Y) (y:X+Y) := 
   match (x,y) with 
   | (inl x0, inl y0) => A x0 y0 
@@ -250,7 +250,7 @@ Definition sum_lift (X Y:Set)
 (*=End *)
 
 (*=LeftSumLift *)
-Definition left_sum_lift (X Y:Set) (A:X->X->Prop) (x y : X+Y) := 
+Definition left_sum_lift (X Y:Type) (A:X->X->Prop) (x y : X+Y) := 
   match (x,y) with 
   | (inl x0, inl y0) => A x0 y0 
   | (inl x0, inr y0) => False 
@@ -259,7 +259,7 @@ Definition left_sum_lift (X Y:Set) (A:X->X->Prop) (x y : X+Y) :=
   end.
 (*=End *)
 
-(* Fixpoint left_sum_tree (X Y:Set) (p:WFT X)  *)
+(* Fixpoint left_sum_tree (X Y:Type) (p:WFT X)  *)
 (*   : WFT (X+Y) :=  *)
 (*   match p with  *)
 (*     | ZT => SUP (fun x => SUP (fun y =>  *)
@@ -276,7 +276,7 @@ Definition left_sum_lift (X Y:Set) (A:X->X->Prop) (x y : X+Y) :=
 (*   end. *)
 
 (*=SecLeftSumTree *)
-Lemma af_left_sum (X Y : Set) (A : X -> X -> Prop) : 
+Lemma af_left_sum (X Y : Type) (A : X -> X -> Prop) : 
   @almost_full X A -> @almost_full (X+Y) (left_sum_lift A).
 (*=End *)
 intro afA.
@@ -305,7 +305,7 @@ induction afA.
 Defined.
 
 (*=Transpose *)
-Definition transpose (X Y:Set) (x:X+Y) : Y+X := 
+Definition transpose (X Y:Type) (x:X+Y) : Y+X := 
   match x with 
   | inl x0 => inr _ x0 
   | inr x0 => inl _ x0
@@ -313,19 +313,19 @@ Definition transpose (X Y:Set) (x:X+Y) : Y+X :=
 (*=End *)
 
 (*=RightTranspose *)
-Definition right_sum_lift (X Y:Set) (B:Y->Y->Prop) (x y:X+Y) := 
+Definition right_sum_lift (X Y:Type) (B:Y->Y->Prop) (x y:X+Y) := 
   left_sum_lift B (transpose x) (transpose y).
 (*=End *)
 
 (*=SecRightSumTree *)			      
-Lemma af_right_sum (X Y : Set) (B : Y -> Y -> Prop) : 
+Lemma af_right_sum (X Y : Type) (B : Y -> Y -> Prop) : 
   @almost_full Y B -> @almost_full (X+Y) (right_sum_lift B).
 intros. unfold right_sum_lift. eapply af_cofmap. apply af_left_sum. apply H.
 Defined.
 (*=End *)
 
 (*=AfSumLift *)
-Corollary af_sum_lift (X Y : Set) : 
+Corollary af_sum_lift (X Y : Type) : 
   forall (A : X -> X -> Prop) (B : Y -> Y -> Prop), 
   almost_full A -> almost_full B -> almost_full (sum_lift A B).
 (*=End *)
@@ -341,7 +341,7 @@ Defined.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
 
 (* Finite natural values in the range [0 ... k-1] that is, k inhabitants *)
-Inductive Finite (k:nat) : Set := FinIntro x (_ : x < k).
+Inductive Finite (k:nat) : Type := FinIntro x (_ : x < k).
 
 Definition next_fin k (x : Finite k) (y : Finite k) := 
   match (x,y) with 
